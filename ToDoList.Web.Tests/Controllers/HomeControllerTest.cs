beff4ffdfba8dab56dc0190d5b6e03f924ce34ca
mkjordanov <mkjordanov@gmail.com>
2017-03-15 20:56:10 +0200

    jenkins test2

--- ToDoList.Web.Tests/Controllers/HomeControllerTest.cs
+++ ToDoList.Web.Tests/Controllers/HomeControllerTest.cs
@@ -18,7 +18,7 @@ namespace ToDoList.Web.Tests.Controllers
         [Test]
         public void SampleTest3()
         {
-            Assert.IsTrue(true);
+            Assert.IsTrue(false);
         }
     }
 }
