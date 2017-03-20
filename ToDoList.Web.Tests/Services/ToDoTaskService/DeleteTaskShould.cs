a6e8d4f29c7b33d6b3e3999693dd0f9653103486
mkjordanov <mkjordanov@gmail.com>
2017-03-20 17:15:02 +0200

    Added tests for DeteTask method in ToDoListTaskService

---
+++ ToDoList.Web.Tests/Services/ToDoTaskService/DeleteTaskShould.cs
@@ -0,0 +1,72 @@
+﻿using Moq;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ToDoList.Data.EFRepository;
+using ToDoList.Data.UnitOfWork;
+using ToDoList.Models;
+using ToDoList.Services;
+
+namespace ToDoList.Web.Tests.Services.ToDoTaskService
+{
+    [TestFixture]
+    public class DeleteTaskShould
+    {
+        [Test]
+        public void Throw_WhenTaskIsNull()
+        {
+
+            //Arrange
+            var mockedToDoListTaskRepository = new Mock<IEFGenericRepository<ToDoListTask>>();
+            var mockedUnitOfWork = new Mock<IUnitOfWork>();
+
+            var taskService = new ToDoListTaskService(mockedToDoListTaskRepository.Object, mockedUnitOfWork.Object);
+
+            //Act & Assert
+
+            Assert.Throws<ArgumentNullException>(() =>
+            {
+                taskService.DeleteTask(null);
+            });
+
+        }
+
+        [Test]
+        public void CallRepositoryDelete_OnlyOnce()
+        {
+
+            //Arrange
+            var mockedToDoListTaskRepository = new Mock<IEFGenericRepository<ToDoListTask>>();
+            var mockedUnitOfWork = new Mock<IUnitOfWork>();
+            var mockedToDoTask = new Mock<ToDoListTask>();
+
+            var taskService = new ToDoListTaskService(mockedToDoListTaskRepository.Object, mockedUnitOfWork.Object);
+
+            //Act
+            taskService.DeleteTask(mockedToDoTask.Object);
+
+            //Assert
+            mockedToDoListTaskRepository.Verify(r => r.Delete(It.IsAny<ToDoListTask>()), Times.Once);
+        }
+
+        [Test]
+        public void CallUnitOfWorkCommit_OnlyOnce()
+        {
+            //Arrange
+            var mockedToDoListTaskRepository = new Mock<IEFGenericRepository<ToDoListTask>>();
+            var mockedUnitOfWork = new Mock<IUnitOfWork>();
+            var mockedToDoTask = new Mock<ToDoListTask>();
+
+            var taskService = new ToDoListTaskService(mockedToDoListTaskRepository.Object, mockedUnitOfWork.Object);
+
+            //Act
+            taskService.DeleteTask(mockedToDoTask.Object);
+
+            //Assert
+            mockedUnitOfWork.Verify(r => r.Commit(), Times.Once);
+        }
+    }
+}
