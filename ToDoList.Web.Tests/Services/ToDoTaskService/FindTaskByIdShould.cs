d735565423c38d1f213ef3aa7851660b924fd2f5
mkjordanov <mkjordanov@gmail.com>
2017-03-21 11:01:54 +0200

    Added tests for FindTaskById method in ToDoTaskService

---
+++ ToDoList.Web.Tests/Services/ToDoTaskService/FindTaskByIdShould.cs
@@ -0,0 +1,90 @@
+﻿using Moq;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ToDoList.Data.EFRepository;
+using ToDoList.Data.UnitOfWork;
+using ToDoList.Models;
+using ToDoList.Services;
+
+namespace ToDoList.Web.Tests.Services.ToDoTaskService
+{
+    [TestFixture]
+    public  class FindTaskByIdShould
+    {
+        [Test]
+        public void Throw_WheTaskIdIsNull()
+        {
+            //Arrange
+            var mockedToDoListTaskRepository = new Mock<IEFGenericRepository<ToDoListTask>>();
+            var mockedUnitOfWork = new Mock<IUnitOfWork>();
+
+            var taskService = new ToDoListTaskService(mockedToDoListTaskRepository.Object, mockedUnitOfWork.Object);
+
+            //Act & Assert
+
+            Assert.Throws<ArgumentNullException>(() =>
+            {
+                taskService.FindTaskById(null);
+            });
+        }
+
+        [Test]
+        public void CallRepositoriesGetById_OnlyOnce()
+        {
+            //Arrange
+            var mockedToDoListTaskRepository = new Mock<IEFGenericRepository<ToDoListTask>>();
+            var mockedUnitOfWork = new Mock<IUnitOfWork>();
+
+            var taskService = new ToDoListTaskService(mockedToDoListTaskRepository.Object, mockedUnitOfWork.Object);
+
+            //Act
+            taskService.FindTaskById(Guid.NewGuid());
+
+            //Assert
+            mockedToDoListTaskRepository.Verify(r => r.GetById(It.IsAny<Guid>()), Times.Once);
+        }
+
+        [Test]
+        public void ReturnsCorrectType_WhenGivenValidParameters()
+        {
+            //Arrange
+            var mockedToDoListTaskRepository = new Mock<IEFGenericRepository<ToDoListTask>>();
+            var mockedUnitOfWork = new Mock<IUnitOfWork>();
+
+            mockedToDoListTaskRepository.Setup(r => r.GetById(It.IsAny<Guid>())).Returns(new ToDoListTask());
+
+            var taskService = new ToDoListTaskService(mockedToDoListTaskRepository.Object, mockedUnitOfWork.Object);
+
+            //Act
+            var actualResult=taskService.FindTaskById(Guid.NewGuid());
+
+            //Assert
+            Assert.That(actualResult, Is.InstanceOf<ToDoListTask>());
+        }
+        [Test]
+
+        public void Retrun_WheGiveValidParameters()
+        {
+            //Arrange
+            var mockedToDoListTaskRepository = new Mock<IEFGenericRepository<ToDoListTask>>();
+            var mockedUnitOfWork = new Mock<IUnitOfWork>();
+
+            var expectedTask = new ToDoListTask() {Task="sample task" };
+
+            mockedToDoListTaskRepository.Setup(r => r.GetById(It.IsAny<Guid>())).Returns(expectedTask);
+
+            var taskService = new ToDoListTaskService(mockedToDoListTaskRepository.Object, mockedUnitOfWork.Object);
+
+            //Act
+            var actualResult = taskService.FindTaskById(Guid.NewGuid());
+
+            //Assert
+            Assert.That(actualResult, Is.EqualTo(expectedTask));
+
+        }
+    }
+}
