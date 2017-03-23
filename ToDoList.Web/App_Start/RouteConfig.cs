ad3bd527de80a4ff3d953999796d2928a38f4f89
mkjordanov <mkjordanov@gmail.com>
2017-03-23 17:26:10 +0200

    URLs are now lowerCase

--- ToDoList.Web/App_Start/RouteConfig.cs
+++ ToDoList.Web/App_Start/RouteConfig.cs
@@ -11,6 +11,7 @@ namespace ToDoList.Web
     {
         public static void RegisterRoutes(RouteCollection routes)
         {
+            routes.LowercaseUrls = true;
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
             routes.MapRoute(
